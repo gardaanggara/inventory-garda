28fe52341c8f83e67899d96485a8d9121e6e1c7e
gardaanggara <garda.anggara90@gmail.com>
2021-10-12 14:15:47 +0700

    latihan routing

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,50 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function() {
+    echo "hellow";
+});
+
+//
+Route::get('/hi/{garda}', function($name) {
+    echo "hellow " . $name;
+});
+
+//Default Parameter
+Route::get('/data/{data?}', function($data="Kosong"){
+    return "Isi Parameter = " . $data;
+});
+
+//Regular Expression
+Route::get('/name/{nama}', function($nama){
+    return "Hellow " .$nama;
+})->where('nama', '[A-Za-z]+');
+
+//Regular Expression Menggunakan Array jika Nama hanya dimasukan huruf dan nrp dimasukan angka
+Route::get('/mahasiswa/{nrp}/{nama}', function($nama, $nrp){
+    echo "Selamat datang ".$nrp. " " .$nama;
+})->where([
+    'nama', '[A-Za-z]+',
+    'nrp', '[0-9]+',
+]);
+
+//Pengecekan Bilangan
+Route::get('/cekbilangan/{bilangan}', function($bilangan){
+  if ($bilangan %2 == 0) {
+      echo "{$bilangan} Bilangan Anda Genap";
+  }else {
+      echo "{$bilangan} Bilangan Anda Ganjil";
+  }
+});
+
+
+Route::get('/deretbilangan/{deret}', function($deret){
+    for ($i=0; $i <= $deret; $i++) { 
+        if ($i %2 == 0) {
+            echo "{$i}Bilangan Genap";
+        }else {
+            echo "{$i}Bilangan Ganjil";
+        }
+    }
+});
